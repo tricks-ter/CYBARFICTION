b6eb977c3e5ceb4105be7cde6e5be5d3a561859a
tricks-ter <84240938+tricks-ter@users.noreply.github.com>
2023-05-02 17:01:30 +0600

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
  const app =  express()
 
- const parser= require("body-parser")
+
 
  const path = require("path")
 
@@ -11,7 +11,7 @@
 
 app.use(express.static(path.join(__dirname,"/public")))
 
- app.use(parser.urlencoded({extended:true}))
+ 
 
 
 app.get("/",function (req, res) {
